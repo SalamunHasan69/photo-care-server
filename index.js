ed49d5de796a13115d1d88d440f03279d5dca409
SalamunHasan69e <salamun2600@gmail.com>
2022-11-08 12:35:01 +0600

    basic mongo setup

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const cors = require('cors');
+var MongoClient = require('mongodb').MongoClient;
 const app = express();
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
+
+
+
+
+var uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ac-aszh8hu-shard-00-00.3kn3bwn.mongodb.net:27017,ac-aszh8hu-shard-00-01.3kn3bwn.mongodb.net:27017,ac-aszh8hu-shard-00-02.3kn3bwn.mongodb.net:27017/?ssl=true&replicaSet=atlas-yt8a18-shard-0&authSource=admin&retryWrites=true&w=majority`;
+console.log(uri);
+MongoClient.connect(uri, function (err, client) {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
 
 app.get('/', (req, res) => {
   res.send('Photo Care Server is Running')
