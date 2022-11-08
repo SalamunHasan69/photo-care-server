edac539c2d1a85bbf559c4c04f14963f712b885d
SalamunHasan69e <salamun2600@gmail.com>
2022-11-08 18:35:01 +0600

    load data from mongoDB

--- index.js
+++ index.js
@@ -12,14 +12,30 @@ app.use(express.json());
 
 
 var uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ac-aszh8hu-shard-00-00.3kn3bwn.mongodb.net:27017,ac-aszh8hu-shard-00-01.3kn3bwn.mongodb.net:27017,ac-aszh8hu-shard-00-02.3kn3bwn.mongodb.net:27017/?ssl=true&replicaSet=atlas-yt8a18-shard-0&authSource=admin&retryWrites=true&w=majority`;
-console.log(uri);
 MongoClient.connect(uri, function (err, client) {
   const collection = client.db("test").collection("devices");
   // perform actions on the collection object
-  client.close();
+  try {
+    const serviceCollection = client.db('photoCare').collection('services');
+
+    app.get('/services', async (req, res) => {
+      const query = {}
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    })
+
+  }
+
+  finally {
+
+  }
 });
 
+async function run() {
 
+}
+run().catch(error => console.error(error));
 
 app.get('/', (req, res) => {
   res.send('Photo Care Server is Running')
